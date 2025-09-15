9d687be853e4dec0fcddaea55251d97d5401f57e
OzoakRH <kanekineki50@gmail.com>
2025-09-15 21:54:33 +0700

    Create to25.py

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,12 @@
+try:
+    num = int(input("Enter a number less than 25\n"))
+except ValueError:
+    print("Error")
+    exit()
+
+if num > 25:
+    print("Error")
+else:
+    while num <= 25:
+        print(f"Inside the loop, my variable is {num}")
+        num += 1
