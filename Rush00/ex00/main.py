fc63bd65e719b075bd674c7e8af1bf56da57230f
OzoakRH <kanekineki50@gmail.com>
2025-09-18 20:46:34 +0700

    Update main.py

--- Rush00/ex00/main.py
+++ Rush00/ex00/main.py
@@ -1,1 +1,14 @@
+from checkmate import checkmate
 
+def main():
+    #จะแสดงตัวอย่างบอร์ด 4x4
+    board_4x4 = """\
+....
+.K..
+..P.
+....\
+""" # K อยู่ที่ (1,1) และ เบี้ย หรือ Pawn อยุ่ที่ (2,2) (Pawn อยู่ใต้ King มุมขวา -> check K)
+    print("=== 4x4 board ===")
+    print(board_4x4)
+    print("Result:")
+    checkmate(board_4x4)
