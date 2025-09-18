da849458d9fdb67102d4301616f745ece090ad07
OzoakRH <kanekineki50@gmail.com>
2025-09-18 20:25:29 +0700

    Create scope_that.py

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,10 @@
+def add_one(number):
+
+    return number + 1
+
+my_variable = 10
+print(f"Original value: {my_variable}")
+
+add_one(my_variable)
+
+print(f"Value after calling add_one: {my_variable}")
